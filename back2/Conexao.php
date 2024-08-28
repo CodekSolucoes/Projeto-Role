39fffef5e72c24fe2cb56d9591d4bbd51cb6ed7a
fabiolealsc <fabio.leal.sc@gmail.com>
2024-08-28 09:23:51 -0300

    Mudança básica no PDO

--- back2/Conexao.php
+++ back2/Conexao.php
@@ -1,4 +1,4 @@
 <?php
 
     $configs = require 'DatabaseConfig.php';
-    $pdo = new PDO($configs['DNS']);
+    $pdo = new PDO($configs['DNS'], $configs['user'], $configs['pass'], $configs['options']);
